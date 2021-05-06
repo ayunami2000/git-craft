a63cad819eda276ed6e4dedce51f4809eaecb662
Sam Williams <spwilliamsiam@gmail.com>
2021-05-06 15:40:58 -0400

    bruh

--- server.sh
+++ server.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 ./ngrok authtoken $NGROK
 ./ngrok tcp 25565 &
+sleep 5s
 url=$(curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url)
 echo $url
 java -Xmx4096M -Xms4096M -jar minecraft_server.jar nogui
