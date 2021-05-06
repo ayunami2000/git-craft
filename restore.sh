e015aab5a977a03134a95161aed03d92f6b46722
Sam Williams <spwilliamsiam@gmail.com>
2021-05-06 16:02:47 -0400

    bruh

--- restore.sh
+++ restore.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 wget -O minecraft_server.jar https://papermc.io/api/v1/paper/1.16.5/latest/download
-wget https://github.com/ayunami2000/git-craft/raw/master/server.zip
+#wget https://github.com/ayunami2000/git-craft/raw/master/server.zip
 unzip server.zip
 echo eula=true>eula.txt
-wget https://github.com/ayunami2000/git-craft/raw/master/pktriot
+#wget https://github.com/ayunami2000/git-craft/raw/master/pktriot
 sudo chmod +x pktriot
 echo $PKTRIOT>pktriot.json
