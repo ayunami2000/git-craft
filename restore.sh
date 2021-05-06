eab49ced56602636987cbb2edf29dfe7661374c3
Sam Williams <spwilliamsiam@gmail.com>
2021-05-06 16:27:31 -0400

    bruh

--- restore.sh
+++ restore.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 wget -O minecraft_server.jar https://papermc.io/api/v1/paper/1.16.5/latest/download
-#wget https://github.com/ayunami2000/git-craft/raw/master/server.zip
 unzip server.zip
+wget -O plugins/IllegalStack.jar https://api.spiget.org/v2/resources/44411/download
 echo eula=true>eula.txt
-#wget https://github.com/ayunami2000/git-craft/raw/master/pktriot
 sudo chmod +x pktriot
 echo $PKTRIOT>pktriot.json
