e7fe88823b72db61027f8d78712b9665ed423c07
ayunami2000 <spwilliamsiam@gmail.com>
2021-05-07 18:21:46 -0400

    Update server.sh

--- server.sh
+++ server.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-./pktriot --config ./pktriot.json start > /dev/null &
 echo Starting!
-java -Xmx4096M -Xms4096M -jar minecraft_server.jar nogui > /dev/null
+java -Xmx4096M -Xms4096M -jar minecraft_server.jar nogui > /dev/null &
+./pktriot --config ./pktriot.json start
