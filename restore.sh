18314a9eb31e2c930983693dd6404fa1af5f0121
Sam Williams <spwilliamsiam@gmail.com>
2021-05-12 15:32:42 -0400

    bruh

--- restore.sh
+++ restore.sh
@@ -3,5 +3,10 @@ wget -O minecraft_server.jar https://papermc.io/api/v1/paper/1.16.5/latest/downl
 unzip server.zip
 wget -O plugins/IllegalStack.jar https://api.spiget.org/v2/resources/44411/download
 echo eula=true>eula.txt
-sudo chmod +x pktriot
-echo $PKTRIOT>pktriot.json
+unzip ayunalt.git-craft.pem.zip
+rm -rf ayunalt.git-craft.pem.zip
+mkdir ~/.ssh/
+mv ayunalt.git-craft.pem ~/.ssh/
+chmod 600 ~/.ssh/ayunalt.git-craft.pem
+#sudo chmod +x pktriot
+#echo $PKTRIOT>pktriot.json
--- server.sh
+++ server.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+#./pktriot --config ./pktriot.json start > /dev/null &
+ssh -o StrictHostKeychecking=no -i ~/.ssh/ayunalt.git-craft.pem ayunalt.git-craft@portmap.io -f -N -R 23063:localhost:25565 > /dev/null &
 echo Starting!
-java -Xmx4096M -Xms4096M -jar minecraft_server.jar nogui > /dev/null &
-./pktriot --config ./pktriot.json start
+java -Xmx4096M -Xms4096M -jar minecraft_server.jar nogui > /dev/null
