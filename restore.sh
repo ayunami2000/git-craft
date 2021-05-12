99bb4f669bcb3234ae2cb84af68913907a50a1c9
Sam Williams <spwilliamsiam@gmail.com>
2021-05-12 17:14:13 -0400

    bruh

--- restore.sh
+++ restore.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 wget -O minecraft_server.jar https://papermc.io/api/v1/paper/1.16.5/latest/download
 unzip server.zip
-wget -O plugins/IllegalStack.jar https://api.spiget.org/v2/resources/44411/download
+#wget -O plugins/IllegalStack.jar https://api.spiget.org/v2/resources/44411/download
 echo eula=true>eula.txt
-unzip -P $PEM ayunalt.git-craft.pem.zip
-rm -rf ayunalt.git-craft.pem.zip
+unzip -P $PEM pem.zip
+rm -rf pem.zip
 mkdir ~/.ssh/
 mv ayunalt.git-craft.pem ~/.ssh/
 chmod 600 ~/.ssh/ayunalt.git-craft.pem
