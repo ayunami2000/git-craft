269e9b899a4f5a19ab65cfb3a9f521c6b8eb766c
Sam Williams <spwilliamsiam@gmail.com>
2021-05-12 22:28:01 -0400

    bruh

--- server.sh
+++ server.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 #./pktriot --config ./pktriot.json start > /dev/null &
-while true; do
-    ssh -o StrictHostKeychecking=no -i ~/.ssh/ayunalt.git-craft.pem ayunalt.git-craft@portmap.io -f -N -R 23063:localhost:25565 > /dev/null
-done &
+ssh -o StrictHostKeychecking=no -i ~/.ssh/ayunalt.git-craft.pem ayunalt.git-craft@portmap.io -f -N -R 23063:localhost:25565
 echo Starting!
 for i in {1..10}; do
     java -Xmx4096M -Xms4096M -jar minecraft_server.jar nogui > /dev/null
